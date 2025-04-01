76fcf245f0eba2ad1fc3b093c44eb550d852dda0
devambarbhaya <barbhaya.devam@gmail.com>
2025-04-01 01:15:42 +0000

    Refactor

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -15,12 +17,6 @@ def main():
         print(f"The '{item['char']}' character was found {item['num']} times")
 
     print("--- End report ---")
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
 
 def sort_on(d):
     return d["num"]
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
